9becbc08e8e5898cb5e472c1938e69e0143d70ff
DESKTOP-O9V6V35\Muhammad ilyas <ilyas>
2018-04-20 10:56:04 +0500

    update server.js

--- server.js
+++ server.js
@@ -3,4 +3,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
+  
+
 app.listen(process.env.PORT || 8080);
