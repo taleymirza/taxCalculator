5cddef6eb72c70ac39ac3fbfb7c976c70fc85d57
DESKTOP-O9V6V35\Muhammad ilyas <ilyas>
2018-04-25 15:57:05 +0500

    comment again

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname + '/dist')));
 
-/*app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});*/
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/src/index.html'));
+});
   
 
   app.listen(process.env.PORT||3000, () => console.log('Example app listening on port 3000!'))
