6ec7465e181cf0ae38da3d9522050a491db1e44a
DESKTOP-O9V6V35\Muhammad ilyas <ilyas>
2018-04-25 11:41:55 +0500

    node server deploy

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-import express from "express";
+const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname + '/dist')));
 
-app.get('/', function(req, res) {
+/*app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
-  });
+});*/
   
 
-app.listen(process.env.PORT || 8080);
+  app.listen(3000, () => console.log('Example app listening on port 3000!'))
